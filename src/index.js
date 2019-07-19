831624d3ef0cde142f3b79750cfa742c9250867d
JasonDollar <elrey33@gmail.com>
2019-07-19 14:07:19 +0200

    set redux

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
-import './index.css'
+import { Provider } from 'react-redux'
+
 import App from './App' 
+import './index.css'
+import store from './redux/store'
 import * as serviceWorker from './serviceWorker'
 
 const app = (
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+
+  </Provider>
 )
 
 ReactDOM.render(app, document.getElementById('root'))
