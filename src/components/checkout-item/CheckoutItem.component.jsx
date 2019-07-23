d1ef94a746531ec2b87aa33943b4178f36c1c367
JasonDollar <elrey33@gmail.com>
2019-07-23 17:22:06 +0200

    added addItem and removeItem to Checkout

--- src/components/checkout-item/CheckoutItem.component.jsx
+++ src/components/checkout-item/CheckoutItem.component.jsx
@@ -1,24 +1,54 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
 
 import './CheckoutItem.styles.scss'
 
+import { clearItemFromCart, addItem, removeItem } from '../../redux/cart/cart.actions'
+
 const CheckoutItem = ({
-  cartItem: { 
-    name, imageUrl, price, quantity,
-  }, 
+  cartItem, 
+  clearItemFromCart,
+  addItem,
+  removeItem,
 }) => {
-  let i
+  const { 
+    name, imageUrl, price, quantity,
+  } = cartItem
   return (
     <div className="checkout-item">
       <div className="image-container">
-        <img src={imageUrl} alt="item image" />
+        <img src={imageUrl} alt="item" />
       </div>
       <span className="name">{name}</span>
-      <span className="quantity">{quantity}</span>
+      <span className="quantity">
+        <div className="arrow" onClick={() => removeItem(cartItem)}>&#10094;</div>
+          <span className="value">{quantity}</span>
+        <div className="arrow" onClick={() => addItem(cartItem)}>&#10095;</div>
+      </span>
       <span className="price">{price}</span>
-      <div className="remove-button">&#10005;</div>
+      <div className="remove-button" onClick={() => clearItemFromCart(cartItem)}>&#10005;</div>
     </div>
   )
 }
 
-export default CheckoutItem
+const mapDispatchToProps = {
+  clearItemFromCart,
+  addItem,
+  removeItem,
+}
+
+export default connect(null, mapDispatchToProps)(CheckoutItem)
+
+
+CheckoutItem.propTypes = {
+  cartItem: PropTypes.shape({
+    name: PropTypes.string, 
+    imageUrl: PropTypes.string, 
+    price: PropTypes.number, 
+    quantity: PropTypes.number,
+  }).isRequired,
+  clearItemFromCart: PropTypes.func.isRequired,
+  addItem: PropTypes.func.isRequired,
+  removeItem: PropTypes.func.isRequired,
+}
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
 import actionTypes from './cart.types'
-import { addItemToCart } from './cart.utils'
+import { addItemToCart, removeItemFromCart } from './cart.utils'
 
 const initialState = {
   hidden: true,
@@ -13,6 +13,13 @@ export default (state = initialState, { type, payload }) => {
       return { ...state, hidden: !state.hidden }
     case actionTypes.ADD_ITEM:
       return { ...state, cartItems: addItemToCart(state.cartItems, payload) }
+    case actionTypes.REMOVE_ITEM:
+      return { ...state, cartItems: removeItemFromCart(state.cartItems, payload) }
+    case actionTypes.CLEAR_ITEM_FROM_CART:
+      return { 
+        ...state,
+        cartItems: state.cartItems.filter(item => item.id !== payload.id),
+      }
     default:
       return state
   }
