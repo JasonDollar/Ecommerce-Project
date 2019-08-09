e5012428adc2ac194700319f2bb73b417321e168
JasonDollar <elrey33@gmail.com>
2019-08-09 13:13:22 +0200

    implement code that pulls data from firebase withSpinner HOC

--- src/pages/shop/Shop.component.jsx
+++ src/pages/shop/Shop.component.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Route } from 'react-router-dom'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
@@ -7,19 +7,25 @@ import { updateCollections } from '../../redux/shop/shop.actions'
 import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils'
 import CollectionsOverview from '../../components/collections-overview/CollectionsOverview.component'
 import CollectionPage from '../collection/Collection.component'
+import WithSpinner from '../../components/with-spinner/WithSpinner.component'
+
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview)
+const CollectionPageWithSpinner = WithSpinner(CollectionPage)
 
 const ShopPage = ({ match, updateCollections }) => {
+  const [loading, toggleLoading] = useState(true)
   useEffect(() => {
     const collectionRef = firestore.collection('collection')
     collectionRef.onSnapshot(async snapshot => {
       const collectionsMap = convertCollectionsSnapshotToMap(snapshot)
       updateCollections(collectionsMap)
+      toggleLoading(false)
     })
   }, [])
   return (
     <div className="shop-page">
-      <Route exact path={`${match.path}`} component={CollectionsOverview} />
-      <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+      <Route exact path={`${match.path}`} render={props => <CollectionsOverviewWithSpinner {...props} isLoading={loading} />} />
+      <Route path={`${match.path}/:collectionId`} component={CollectionPage} render={props => <CollectionPageWithSpinner {...props} isLoading={loading} />} />
     </div>
   )
 }
